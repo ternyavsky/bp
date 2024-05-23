e0f7b1042a187dc441e052ef87d689fe9ea56e7e
ternyavsky <ilya.ternyavsky@yandex.ru>
2024-05-23 21:10:43 +0300

    users

--- src/config/settings.py
+++ src/config/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "config.middleware.CORSMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
